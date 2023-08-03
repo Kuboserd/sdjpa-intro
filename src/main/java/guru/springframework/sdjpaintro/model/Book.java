401b62ec3a40435bbd13c2d3186b02a90c72737e
Kuboserd <Kuboserd@gmail.com>
2023-08-03 15:23:45 +0200

    First leesson.

---
+++ src/main/java/guru/springframework/sdjpaintro/model/Book.java
@@ -0,0 +1,49 @@
+package guru.springframework.sdjpaintro.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String isbn;
+    private String publisher;
+
+    public Book(String name, String isbn, String publisher) {
+        this.name = name;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
