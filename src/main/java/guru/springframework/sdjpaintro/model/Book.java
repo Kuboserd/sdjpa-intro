53320e1a0fbe05de500366c3efc35de328892093
Kuboserd <Kuboserd@gmail.com>
2023-08-03 15:29:00 +0200

    Hash and equals functions

--- src/main/java/guru/springframework/sdjpaintro/model/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/model/Book.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Book {
     @Id
@@ -13,6 +15,21 @@ public class Book {
     private String name;
     private String isbn;
     private String publisher;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 
     public Book(String name, String isbn, String publisher) {
         this.name = name;
